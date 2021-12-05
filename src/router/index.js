22e8d141c2ba88604e309f8b18c3bf2017d12af7
Edwing Romero <edwing8957@gmail.com>
2021-12-04 22:22:46 -0500

    usar state para los datos de usuario y token

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -8,22 +9,63 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      allowAnonymous: true
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'Login',
+    meta: {
+      allowAnonymous: true
+    },
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/auth',
+    component: () => import('@/views/auth/Template.vue'),
+    meta: {
+      allowAnonymous: false
+    },
+    children: [
+      { path: 'cursos', component: () => import('@/views/auth/Cursos.vue') },
+      { path: 'autores', component: () => import('@/views/auth/Autores.vue') }
+    ]
+  },
 ]
+
+
+
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+
+
+
+router.beforeEach((to, from, next) => {
+  
+  //YA ESTA LOGUEADO
+  if(to.fullPath=='/login' && store.state.token!=''){
+    next({
+      path: '/auth/cursos'
+    })
+  }
+
+  //SI NECESITA CREDENCIAL LO REGRESA AL LOGIN
+  if (!to.meta.allowAnonymous && store.state.token=='') {
+    next({
+      path: '/login'
+    })
+  }
+  else {
+    next()
+  }
+  //next()
+})
+
 
 export default router
