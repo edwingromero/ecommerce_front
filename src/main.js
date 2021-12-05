a7b3e07d1069a2d103109b879df0788969d6f83b
Edwing Romero <edwing8957@gmail.com>
2021-12-04 22:24:22 -0500

    configuracion de axios - interceptor refresh

--- src/main.js
+++ src/main.js
@@ -1,12 +1,45 @@
-window.axios = require('axios');
-window.axios.defaults.baseURL = 'https://ecommerce_back.test/api';
-
-window._ = require('lodash');
+require('./bootstrap');
 
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+
+
+
+import axios from 'axios';
+
+
+Vue.prototype.axios = axios;
+
+axios.defaults.baseURL = 'https://ecommerce_back.test/api';
+
+let storage = localStorage.getItem('vuex') ? JSON.parse(localStorage.getItem('vuex')) : null;
+if(storage!=null && storage.token != ''){
+  axios.defaults.headers.common['Authorization'] = 'Bearer '+storage.token.access_token; 
+}
+
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
+
+
+// Function that will be called to refresh authorization
+const refreshAuthLogic = failedRequest => axios.post('https://ecommerce_back.test/api/refresh',{},{
+  headers: { Authorization: `Bearer ${store.state.token.access_token}` }
+}).then(tokenRefreshResponse => {
+    store.commit('setToken',tokenRefreshResponse.data);
+    failedRequest.response.config.headers['Authorization'] = 'Bearer ' + tokenRefreshResponse.data.access_token;
+    return Promise.resolve();
+});
+
+// Instantiate the interceptor
+createAuthRefreshInterceptor(axios, refreshAuthLogic);
+
+
+// Make a call. If it returns a 401 error, the refreshAuthLogic will be run, 
+// and the request retried with the new token
+
+
 
 
 
