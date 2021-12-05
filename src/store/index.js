dbd3681ff733bf2a033c5a7b9b147a7c859f2ed2
Edwing Romero <edwing8957@gmail.com>
2021-12-04 22:23:19 -0500

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user:{},
+    token:''
   },
   mutations: {
+    setToken(state,payload){
+      state.token = payload;
+    },
+    setUser(state,payload){
+      state.user = payload;
+    },
+    logOut(state){
+      state.user = {};
+      state.token = '';
+    }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [createPersistedState()],
 })
